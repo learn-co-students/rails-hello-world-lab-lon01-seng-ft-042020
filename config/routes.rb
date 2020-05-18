f9f95ebef563784bc6aa9a0f3ab889b88c4dce34
github username <github email address>
2020-05-18 02:41:28 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ #Integrate a route for localhost:3000/hello_world
+ get "/hello_world", to: "hw#hello_world"
 end
